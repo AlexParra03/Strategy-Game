0f8ac1af61d626661a529aaaf08d669d83f1a70a
David Alejandro Parra <AlexParra03@users.noreply.github.com>
2017-07-13 16:16:40 -0400

    Update sendattack.php

--- sendattack.php
+++ sendattack.php
@@ -6,7 +6,7 @@ echo "<p>" . $defenderNAME . "</p>";
 session_start();
 $userID = $_SESSION['ID'];
 		
-$connection = mysql_connect("mysql2.000webhost.com","a6173051_admin","camote2434") or die("Could not connect to server SQL");
+$connection = mysql_connect("mysql2.000webhost.com","a6173051_admin","") or die("Could not connect to server SQL");
 mysql_select_db("a6173051_data1",$connection) or die("Could not connect to database");
 $query = mysql_query("SELECT * FROM User WHERE ID = '$userID' ");
 $myData = mysql_fetch_array($query);
